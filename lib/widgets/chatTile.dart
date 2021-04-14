c3f13577b3a6b53c9d3d989e8002dcb6adb6f8cf
Richard Joel <richard_joyal@outlook.com>
2021-04-14 18:21:43 +0530

    Last message, time feature

--- lib/widgets/chatTile.dart
+++ lib/widgets/chatTile.dart
@@ -1,9 +1,9 @@
 import 'package:blood_plus/screens/messageScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
-
-
-class ChatTile extends StatelessWidget {
+class ChatTile extends StatefulWidget {
   const ChatTile({
     Key key,
     @required this.connection,
@@ -12,7 +12,65 @@ class ChatTile extends StatelessWidget {
   final connection;
 
   @override
+  _ChatTileState createState() => _ChatTileState();
+}
+
+class _ChatTileState extends State<ChatTile> {
+  var messages;
+  var message;
+  bool isFirst = true;
+
+  void loadMessages(String key) async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    firestore
+        .collection("chats")
+        .doc(key)
+        .collection("messages")
+        .orderBy('timestamp')
+        .snapshots()
+        .listen((event) {
+      var tMessages = [];
+      event.docs.forEach((element) {
+        tMessages.add(element.data());
+        if (tMessages.length == event.size) {
+          setState(() {
+            messages = tMessages;
+            message = messages[event.size-1];
+          });
+        }
+      });
+    });
+  }
+
+  String checkDate() {
+    DateTime checkedTime = DateTime.fromMillisecondsSinceEpoch(
+        message["timestamp"].seconds * 1000);
+    print(DateFormat.jm().format(checkedTime));
+    DateTime currentTime = DateTime.now();
+
+    if ((currentTime.year == checkedTime.year) &&
+        (currentTime.month == checkedTime.month) &&
+        (currentTime.day == checkedTime.day)) {
+      return DateFormat.jm().format(checkedTime);
+    } else if ((currentTime.year == checkedTime.year) &&
+        (currentTime.month == checkedTime.month)) {
+      if ((currentTime.day - checkedTime.day) == 1) {
+        return "YESTERDAY";
+      } else if ((currentTime.day - checkedTime.day) == -1) {
+        return "TOMORROW";
+      } else {
+        return DateFormat.MMMd().format(checkedTime);
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (isFirst) {
+      print("HI");
+      this.loadMessages(widget.connection['key']);
+      isFirst = false;
+    }
     return Column(
       children: [
         Container(
@@ -22,18 +80,35 @@ class ChatTile extends StatelessWidget {
               color: Colors.white,
             ),
             child: GestureDetector(
-              child: ListTile(
-                leading: CircleAvatar(child: Icon(Icons.person), backgroundColor: Colors.grey, foregroundColor: Colors.blueGrey,),
-                title: Text(connection['name'], style: Theme.of(context).textTheme.headline6.copyWith(color: Colors.black, fontSize: 18,),),
-                subtitle: Text('Hello Friend!', style: Theme.of(context).textTheme.headline1.copyWith(fontWeight: FontWeight.normal, fontSize: 14,)),
-                trailing: Text('• Yesteday', style: Theme.of(context).textTheme.headline1.copyWith(fontWeight: FontWeight.normal, fontSize: 13,)),
+                child: ListTile(
+              leading: CircleAvatar(
+                child: Icon(Icons.person),
+                backgroundColor: Colors.grey,
+                foregroundColor: Colors.blueGrey,
+              ),
+              title: Text(
+                widget.connection['name'],
+                style: Theme.of(context).textTheme.headline6.copyWith(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+              ),
+              subtitle: Text(message != null ? '${message["message"]}' : '',
+                  style: Theme.of(context).textTheme.headline1.copyWith(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 14,
+                      )),
+              trailing: Text(message != null ? '${checkDate()}' : '',
+                  style: Theme.of(context).textTheme.headline1.copyWith(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 13,
+                      )),
               onTap: () {
-                Navigator.pushNamed(
-                    context, MessageScreen.routeName,
-                    arguments: connection);
+                Navigator.pushNamed(context, MessageScreen.routeName,
+                    arguments: widget.connection);
               },
             ))),
-      Divider(),      
+        Divider(),
       ],
     );
   }
--- lib/widgets/messageBubble.dart
+++ lib/widgets/messageBubble.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
 class MessageBubble extends StatelessWidget {
   const MessageBubble({
     Key key,
@@ -10,29 +11,50 @@ class MessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-          left: 14, right: 14, top: 10, bottom: 10),
-      child: Align(
-        alignment: (message['sender'] !=
-                FirebaseAuth.instance.currentUser.uid
-            ? Alignment.topLeft
-            : Alignment.topRight),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: (message['sender'] !=
-                    FirebaseAuth.instance.currentUser.uid
-                ? Colors.grey.shade200
-                : Colors.red[300]),
-          ),
-          padding: EdgeInsets.all(16),
-          child: Text(
-            message['message'],
-            style: TextStyle(fontSize: 15),
-          ),
-        ),
-      ),
-    );
+    return Row(
+  mainAxisAlignment: message['sender'] ==  FirebaseAuth.instance.currentUser.uid ? MainAxisAlignment.end : MainAxisAlignment.start,
+  //this will determine if the message should be displayed left or right
+  children: [
+    Flexible(
+    //Wrapping the container with flexible widget
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        margin: EdgeInsets.all(4.0),
+        constraints: BoxConstraints(maxWidth: 350),
+        decoration: BoxDecoration(
+        color: message['sender'] ==  FirebaseAuth.instance.currentUser.uid ? Colors.red[200] : Colors.blue[200],
+        borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Flexible(
+            //We only want to wrap the text message with flexible widget
+              child: Container(
+                child: Text(
+                  message['message'],
+                  style: TextStyle(color: Colors.black, fontWeight: FontWeight.w400),
+                  )
+                )
+              ),    
+              SizedBox(
+                width: 8.0,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 6.0),
+                child: Text(
+                  DateFormat.jm().format(DateTime.fromMillisecondsSinceEpoch(message["timestamp"].seconds * 1000)),
+                  style: TextStyle(fontSize: 10.0, color: Colors.black),
+                  ),
+                ),
+              SizedBox(
+                width: 8.0,
+              ),
+              ],
+            )),
+
+           )
+
+        ]
+        );
   }
 }
